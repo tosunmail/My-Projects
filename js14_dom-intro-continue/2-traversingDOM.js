4d6d7620b921c808cad8db94149032f2117e52de
tosunmail <tosunmaildir@gmail.com>
2023-01-12 19:31:13 +0100

    queryselector

--- js14_dom-intro-continue/2-traversingDOM.js
+++ js14_dom-intro-continue/2-traversingDOM.js
@@ -16,8 +16,32 @@ console.log("**** TRAVERSING DOM ****")
 //* 1- Yukari Yonde traverse
 //*----------------------------------------------
 
+const list  = document.querySelector("li")
+console.log(list);
+console.log(list.parentElement);
+console.log(list.parentNode);
+console.log(list.parentNode.parentNode.parentNode);
+console.log(
+    list.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
+);
+
+console.log(list.closest("section"));
+console.log(list.closest("body").querySelector("header h1"));
+
 //*2- Asagi Yonde traverse
 //*----------------------------------------------
 
+const addItem = document.querySelector(".add-item")
+console.log(addItem.children);
+console.log(addItem.children[0]);
+addItem.children[0].style.color = "red"
 //*3- Yatay Yonde traverse
 //*----------------------------------------------
+const ul = document.querySelector("ul")
+
+console.log(ul);
+
+const lists = ul.children
+console.log(lists);
+const js = lists[2]
+console.log(js.innerText);
