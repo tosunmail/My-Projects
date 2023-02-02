5f4ceff71e0515e5bdc179c6fd33ffb73f108b33
tosunmail <tosunmaildir@gmail.com>
2023-02-02 20:29:18 +0100

    static

--- js22_oop2-modules/4-polymorphism.js
+++ js22_oop2-modules/4-polymorphism.js
@@ -7,4 +7,50 @@
 //* Polymorphism, genellikle Overloading ve Overriding gibi alt kavramlar
 //* ile bilinir.
 
-console.log("private");
+console.log("Polymorphism");
+
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  getSummary() {
+    return `${this.title} was writtten by ${this.author} in ${this.year}`;
+  }
+
+  getAge() {
+    return `${new Date().getFullYear() - this.year}`;
+  }
+  setPrice(price) {
+    const taxRate = 1.1
+    this.price = Math.trunc(price * taxRate)
+  }
+}
+
+class Magazine extends Book {
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  }
+  setPrice(price,taxRate = 1){
+    this.price = Math.trunc(price* taxRate)
+  }
+   getSummary() {
+    return `${this.title} was writtten by ${this.author} in ${this.year} in ${this.month}`;
+   }
+
+   getSummaryParent(){
+    return super.getSummary()
+   }
+}
+const mag1 = new Magazine("SRE", "Einstion", 1930, "Nov");
+console.log(mag1);
+console.log(mag1.getAge());
+console.log(mag1.getSummary());
+console.log(mag1.getSummaryParent());
+mag1.setPrice(100,1.2)
+console.log(mag1);
+
+
--- js22_oop2-modules/5-static-private.js
+++ js22_oop2-modules/5-static-private.js
@@ -22,3 +22,67 @@
 
 //! Private metotlara ise class disirasindan da eriselemez.
 //! Private metotlara ancak class icerisindeki diger metotlar ile erisilebilir.
+
+class Book {
+  // Private Property
+
+//   static veriables are called class veriables too.
+  #id = "123456";
+  static counter = 0
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.#id = "111";
+
+    Book.counter++
+  }
+  getSummary() {
+    return `${this.title} was writtten by ${this.author} in ${this.year}. Before ${this.#computeAge()} years ago`;
+  }
+  // You can read the private veriables with the help of public methods
+
+  getId() {
+    return this.#id;
+  }
+  setId(id) {
+    this.#id = id;
+  }
+  #computeAge(){
+return new Date().getFullYear() - this.year
+  }
+}
+
+Book.prototype.counter = 0
+
+const book1 = new Book("Kasagi", "Ömer Seyfettin", 1920);
+console.log(book1);
+
+// Private degiskenin    degeri class disindan dogrudan okunamaz
+// console.log(book1.#id);
+
+// book1.#id = "123456"
+
+// console.log(book1.getId());
+// console.log(book1.setId("4444"));
+// // console.log(book1.#computetAge());
+// console.log(book1.getSummary());
+
+
+// // static
+// book1.counter++
+// console.log(book1);
+
+// book2.counter++
+// console.log(book2.counter);
+
+
+console.log(book1.counter);
+
+console.log(Book.counter);
+const book2 = new Book("x", "Ömer y", 1440);
+const book3 = new Book("x", "Ömer y", 1440);
+const book4 = new Book("x", "Ömer y", 1440);
+console.log(Book.counter);
+
+
